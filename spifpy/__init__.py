3127a67201320f407eb8795e229750d7f98bbff4
Sergei Shilin <code@shilin.ca>
2022-11-09 11:16:34 -0500

    Introduce SpifDataset and probe classes (#4)
    * Define probe classes
    * Introduce SpifDataset class

--- spifpy/__init__.py
+++ spifpy/__init__.py
@@ -2,8 +2,12 @@
 
 from pkg_resources import DistributionNotFound, get_distribution
 
+from .core.dataset import SpifDataset
+
 try:
     __version__ = get_distribution("spifpy").version
 except DistributionNotFound:
     # package is not installed
     pass
+
+__all__ = ["SpifDataset"]
